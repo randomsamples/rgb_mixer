dbc2a550a641bfe0697a7fee06cc4ee8d6832831
Jason Mackay <randomsamples@users.noreply.github.com>
2021-07-19 19:23:26 -0700

    encoder passing

--- src/encoder.v
+++ src/encoder.v
@@ -1,11 +1,35 @@
 `default_nettype none
 `timescale 1ns/1ns
-module encoder (
+module encoder #(parameter WIDTH = 8,
+                 parameter INC_VALUE = 1) (
     input clk,
     input reset,
     input a,
     input b,
-    output reg [7:0] value
+    output reg [WIDTH - 1:0] value
 );
+
+reg old_a;
+reg old_b;
+
+always @(posedge clk) begin
+    if (reset) begin
+        old_a <= 0;
+        old_b <= 0;
+        value <= 0;
+    end else begin
+        case ({a, old_a, b, old_b})
+            4'b1000: value <= value + INC_VALUE;
+            4'b0111: value <= value + INC_VALUE;
+            4'b0010: value <= value - INC_VALUE;
+            4'b1101: value <= value - INC_VALUE;
+            default:
+                value <= value;
+        endcase
+
+        old_a <= a;
+        old_b <= b;
+    end
+end
 
 endmodule
